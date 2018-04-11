674a6c9647b376cbd30ece41c57a2418aa3325f3
Ian Oberbillig <Ian@Ians-MacBook-Pro-2.local>
2018-04-11 13:16:17 -0600

    added render test for all components

--- App.test.js
+++ App.test.js
@@ -1,9 +1,42 @@
 import React from 'react';
 import App from './App';
+import CreateUser from './Comp/CreateUser.js';
+import Login from './Comp/Login.js';
+import MyPage from './Comp/MyPage.js';
 
 import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const rendered = renderer.create(<App />).toJSON();
-  expect(rendered).toBeTruthy();
+describe('tests for functionality of entire app', () => {
+  it('renders without crashing', () => {
+    const rendered = renderer.create(<App />).toJSON();
+    expect(rendered).toBeTruthy();
+  })
+});
+
+describe('tests for functionality of CreateUser page', () => {
+  it('renders without crashing', () => {
+    const rendered = renderer.create(<CreateUser />).toJSON();
+    expect(rendered).toBeTruthy();
+  })
+});
+
+describe('tests for functionality of Login page', () => {
+  it('renders without crashing', () => {
+    const rendered = renderer.create(<Login />).toJSON();
+    expect(rendered).toBeTruthy();
+  })
+});
+
+describe('tests for functionality of MyPage page', () => {
+  it('renders without crashing', () => {
+    const mockNavigator = {
+      state: {
+        params:{
+          username:"",
+        }
+      }
+    }
+    const rendered = renderer.create(<MyPage navigation = {mockNavigator} />).toJSON();
+    expect(rendered).toBeTruthy();
+  })
 });
