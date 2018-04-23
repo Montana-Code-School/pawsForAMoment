1caa638e0609b3a56c16f8e030d564d944f0c90d
Eric Beau Breitenbach <ebbreitenbach@Erics-MacBook-Air.local>
2018-04-22 18:41:20 -0600

    Mypets almost correctly shows added pets to user. Used code from the pets page in mypets to show the pets, all the pets.

--- Comp/MyPage.js
+++ Comp/MyPage.js
@@ -1,18 +1,148 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Image} from 'react-native';
 
 export default class MyPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      myPets: "",
+      heightVals: [],
+      showVals: []
     }
   }
 
+componentDidMount() {
+  fetch('http://localhost:5000/displayPets/' + this.props.parentState.username)
+    .then((res) => {
+      return res.json();
+    })
+
+    .then((pets) => {
+      this.setState({
+        myPets: pets.pets
+      })
+      console.log(pets.pets[0])
+    })
+
+    .catch((err) => {
+      console.log(err)
+      return err;
+    })
+
+    let boxHeight = [];
+    let show = [];
+    for(var i = 0; i < this.state.myPets; i++) {
+      boxHeight.push(100);
+      show.push("none");
+    }
+    this.setState({ heightVals: boxHeight, showVals: show });
+}
+
   render() {
+    let data = this.state.myPets;
+    let listOfPets = [];
+    for(let i = 0; i < data.length; i++) {
+      listOfPets.push((
+        <TouchableOpacity
+          style={[styles.petButton, {height: this.state.heightVals[i]}]}
+          key={i}
+          onPress={() => {
+            let boxHeight = this.state.heightVals;
+            let show = this.state.showVals;
+            for (var j = 0; j < boxHeight.length; j++) {
+              if (boxHeight[j] == '100%') {
+                boxHeight[j] = 100;
+                show[j] = "none";
+              } else if (i == j) {
+               boxHeight[j] = '100%';
+               show[j] = "flex";
+             } else {
+               boxHeight[j] = 100;
+               show[j] = "none";
+             }
+            }
+            this.setState({
+              heightVals:boxHeight,
+              showVals:show
+            })
+          }}>
+          <Image
+            source={{uri: data[i].image}}
+            style={styles.petImage}
+          />
+          <View style = {styles.petInfoText}>
+            <Text
+              style={styles.petButtonText}>
+              {data[i].petname}
+            </Text>
+            <View style={{
+              display:this.state.showVals[i],
+              paddingLeft: "10%",
+              paddingTop: "10%"
+              }}>
+              <Text>
+                Shelter: {data[i].shelter}
+              </Text>
+              <Text>
+                Location: {data[i].location}
+              </Text>
+              <Text>
+                Breed: {data[i].breed}
+              </Text>
+              <Text>
+                Age: {data[i].age}
+              </Text>
+              <Text>
+                Gender: {data[i].gender}
+              </Text>
+              <Text>
+                Bio: {data[i].bio}
+              </Text>
+            </View>
+          </View>
+        </TouchableOpacity>
+      ))
+    }
     return(
-      <View>
-        <Text>Welcome back, {this.props.parentState.username}!</Text>
+      <ScrollView style={styles.contentContainer}>
+      <View style={{paddingBottom:100}}>
+        {listOfPets}
       </View>
+      </ScrollView>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    paddingVertical: 20,
+  },
+  petButton: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    height: 100,
+    backgroundColor: '#edeeef',
+    marginBottom: 10,
+    paddingBottom: '10%',
+    flexWrap: 'wrap'
+  },
+  petButtonText: {
+    paddingLeft: '35%',
+    paddingTop: '14%',
+    fontSize: 20,
+    fontWeight: 'bold',
+    alignItems: 'center'
+  },
+  petImage: {
+    marginLeft: 10,
+    marginTop: 10,
+    width: 80,
+    height: 80,
+  },
+  petInfoText: {
+    flexDirection: 'column',
+    justifyContent:'flex-start',
+    width:250,
+  }
+});
